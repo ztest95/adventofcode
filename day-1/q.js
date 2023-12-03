c705bbe4d7c0a089de7a415bd23c6b3880f476a8
ztest95 <110767420+ztest95@users.noreply.github.com>
2023-12-03 01:23:30 +0000

    day 2, shortened solution a bit

--- day-1/q.js
+++ day-1/q.js
@@ -21,63 +21,21 @@ let arrNums2 = []
 
 function wordsToNum(word) {
     let w = word
-    let i = w.search(/[0-9]/)
-    let front = w.slice(0, i+1)
 
-    if (front.length > 2) {
-        for (let i = 2; i <= front.length; i++) {
-            let slice = front.slice(i-2, i+1)
-            let slice2 = slice.replace('one', 'o1ne') // added letters at end for edge case like 'twone' = 21
-            slice2 = slice2.replace('two', 't2wo')
-            slice2 = slice2.replace('six', 's6ix')
-            front = front.replace(slice, slice2)
-        }
+    w = w.replaceAll('one', 'o1e')
+    w = w.replaceAll('two', 't2o')
+    w = w.replaceAll('six', 's6x')
+    w = w.replaceAll('four', 'f4r')
+    w = w.replaceAll('five', 'f5e')
+    w = w.replaceAll('nine', 'n9e')
+    w = w.replaceAll('three', 't3e')
+    w = w.replaceAll('seven', 's7n')
+    w = w.replaceAll('eight', 'e8t')
 
-        for (let i = 3; i <= front.length; i++) {
-            let slice = front.slice(i-3, i+1)
-            let slice2 = slice.replace('four', 'fo4ur')
-            slice2 = slice2.replace('five', 'fi5ve')
-            slice2 = slice2.replace('nine', 'ni9ne')
-            front = front.replace(slice, slice2)
-        }
-
-        front = front.replaceAll('three', 't3hree')
-        front = front.replaceAll('seven', 'se7ven')
-        front = front.replaceAll('eight', 'ei8ght')
-        
-    }
-
-    let back = w.slice(i+1, w.length)
-
-    if (back.length > 2) {
-        for (let i = 2; i <= back.length; i++) {
-            let slice = back.slice(i-2, i+1)
-            let slice2 = slice.replaceAll('one', 'o1ne')
-            slice2 = slice2.replaceAll('two', 't2wo')
-            slice2 = slice2.replaceAll('six', 's6ix')
-            back = back.replaceAll(slice, slice2)
-        }
-
-        for (let i = 3; i <= back.length; i++) {
-            let slice = back.slice(i-3, i+1)
-            let slice2 = slice.replaceAll('four', 'f4our')
-            slice2 = slice2.replaceAll('five', 'f5ive')
-            slice2 = slice2.replaceAll('nine', 'n9ine')
-            back = back.replaceAll(slice, slice2)
-        }
-
-        back = back.replaceAll('three', 't3hree')
-        back = back.replaceAll('seven', 's7even')
-        back = back.replaceAll('eight', 'e8ight')
-        
-    }
-
-
-    return front + back
+    return w
 }
 for (let i in arrWords) {
     w = wordsToNum(arrWords[i])
-    console.log(`${arrWords[i]} = ${w}`)
     arrNums2[i] = w.replace(/[^0-9]/g, '') 
 }
 let sum2 = 0
