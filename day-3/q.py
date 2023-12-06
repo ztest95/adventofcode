98645fb1add17eca7f140e718913dd9b5bcd6cb2
ztest95 <110767420+ztest95@users.noreply.github.com>
2023-12-06 12:53:12 +0000

    Refactoring day 3 part 1,/home/codespace/.python/current/bin/python3 /workspaces/adventofcode/day-3/q.py before part 2

--- day-3/q.py
+++ day-3/q.py
@@ -1,146 +1,113 @@
     
-def check_lastline_for_digits(indx: int, line) -> int:
-    index = indx # index starts at top left corner of index of symbol
-    line_sum = 0
-    last_line = line
-    # Top Left Corner Index
-    digit_strings = '.'
+def check_line_for_digits(symbol_index: int, line) -> (str, int):
+    symbol_numbers = 0
+    digit_strings = ''
 
-
+    # Scan Left Digits
     l = 1
-    while last_line[index-l].isdigit():
-        digit_strings = last_line[index-l] + digit_strings
+    while line[symbol_index-l].isdigit():
         l += 1
 
-    line_sum = line_sum if (digit_strings == '.') else line_sum + int(digit_strings[0:-1])
-    digit_strings1 = digit_strings
-    first_len = len(digit_strings)
+    # Scan Right Digits
     r = 1
-    while last_line[index+r].isdigit():
-        digit_strings = digit_strings + last_line[index+r]
+    while line[symbol_index+r].isdigit():
         r += 1
-    second_len = len(digit_strings)
-    line_sum = line_sum if (first_len == second_len) else line_sum + int(digit_strings.replace(digit_strings1, ''))
 
-    if last_line[index].isdigit():
-        digit_strings = digit_strings.replace('.', last_line[index])
-        last_line = list(last_line)
-        last_line[index] = '.'
-        last_line = ''.join(last_line)
-        line_sum = digit_strings.strip('.')
-        line_sum = int(line_sum)
-   
-    digit_strings = digit_strings.strip('.')
+    digit_strings = line[symbol_index-l+1:symbol_index+r]
 
-    last_line = list(last_line)
-    last_line[index-l:index] = ['.' for i in range(l)]
-    last_line[index+1:index+r+1] = ['.' for i in range(r)]
-    last_line = ''.join(last_line)
+    if line[symbol_index].isdigit():
+        # if middle index is digit
+        # digit_strings = digit_strings.replace('.', last_line[symbol_index])
+        symbol_numbers = [int(digit_strings.replace('.', line[symbol_index]))]
+    else:
+        # if middle index is not a digit
+        symbol_numbers = digit_strings.split(".")
+        symbol_numbers = [int(i) for i in symbol_numbers if i != '']
 
-    # # Top Index
+    # Convert Digits to '.'    
+    new_line = list(line)
+    new_line[symbol_index-l+1:symbol_index+r] = ['.' for i in range(l+r-1)]
+    new_line = ''.join(new_line)
 
-    # if last_line[index].isdigit(): 
-    #     digit_strings = ''
+    return new_line , symbol_numbers
 
-    #     k = 0
-    #     while last_line[index+k].isdigit():
-    #         digit_strings = digit_strings + last_line[index+k]
-    #         k += 1
-        
-    #     line_sum = int(digit_strings) + line_sum
+def check_currentline_for_digits(symbol_index, line):
 
-    #     last_line = list(last_line)
-    #     last_line[index] = '.'
-    #     last_line = ''.join(last_line)
-
-    # index += 1
-
-    # # Top Right Index
-    # if last_line[index].isdigit():
-    #     digit_strings = ''
-
-    #     k = 0
-    #     while last_line[index+k].isdigit():
-    #         digit_strings = digit_strings + last_line[index+k]
-    #         k += 1
-
-    #     line_sum = int(digit_strings) + line_sum
-
-    #     last_line = list(last_line)
-    #     last_line[index:index+k] = ['.' for i in range(k)]
-    #     last_line = ''.join(last_line)
-
-    return last_line , line_sum
-
-def check_currentline_for_digits(indx, line):
-    index = indx
-    line_sum = 0
-    current_line = line
-    # Left Index
+    symbol_numbers = 0
     digit_strings = ''
-    k = 1
-    while current_line[index-k].isdigit():
-        digit_strings = current_line[index-k] + digit_strings
-        k += 1
-    
-    line_sum = line_sum if (digit_strings == '') else line_sum + int(digit_strings)
- 
-    current_line = list(current_line)
-    current_line[index-k:index] = ['.' for i in range(k)]
-    current_line = ''.join(current_line)
 
-    digit_strings = ''
-    k = 1
-    while current_line[index+k].isdigit():
-        digit_strings = digit_strings + current_line[index+k]
-        k += 1
-    
-    line_sum = line_sum if (digit_strings == '') else line_sum + int(digit_strings)
+    # Scan Left Digits
+    l = 1
+    while line[symbol_index-l].isdigit():
+        l += 1
 
-    current_line = list(current_line)
-    current_line[index+1:index+k+1] = ['.' for i in range(k)]
-    current_line = ''.join(current_line)
+    # Scan Right Digits
+    r = 1
+    while line[symbol_index+r].isdigit():
+        r += 1
 
-    return current_line , line_sum
-            
+    digit_strings = line[symbol_index-l+1:symbol_index+r]
+
+    # Middle index will always be a digit
+    symbol_numbers = digit_strings.split(line[symbol_index])
+    symbol_numbers = [int(i) for i in symbol_numbers if i != '']
+
+    # Convert Digits to '.'    
+    new_line = list(line)
+    new_line[symbol_index-l+1:symbol_index+r] = ['.' for i in range(l+r-1)]
+    new_line = ''.join(new_line)
+
+    return new_line , symbol_numbers
+
 if __name__ == "__main__":
 
-    arrWords = []
-    with open("day-3/q.txt") as f:
-        for line in f.readlines():
-            arrWords.append(line)
-    
-    
     symbols = ['*', '-', '$', '@', '=', '#', '+', '/', '%', '&']
 
-    q = len(arrWords[0])
-    sum = 0
-    
+    sums = 0
 
+    new_lines = []
 
-    last_line = '.' * q
-    for i in range(len(arrWords)):
-        current_line = arrWords[i]
-        if i != len(arrWords)-1:
-            next_line = arrWords[i+1]
-        else:
-            next_line = '.'
-        for j in range(len(current_line)):
-            if current_line[j] in symbols:
-                # scans last line for digits
-                tup = check_lastline_for_digits(j, last_line)
-                tup2 = check_currentline_for_digits(j, current_line)
-                arrWords[i-1] = tup[0]
-                arrWords[i] = tup2[0]
+    with open("day-3/q.txt") as f:
+        lines = f.readlines()
+        new_lines = lines
 
-                sum += tup2[1] + tup[1]
-                if i != len(arrWords)-1:
-                    tup3 = check_lastline_for_digits(j, next_line)
-                    arrWords[i+1] = tup3[0]
-                    sum += tup3[1]
+        for i, current_line in enumerate(lines):
 
-        last_line = current_line
+            if i < len(lines) - 1:
+                next_line = lines[i+1]
+            else:
+                next_line = '.'
 
-    for x in arrWords:
-        print(x)
-    print(sum)
+            # Don't need this, but this makes the code work incase there is symbol on first line
+            if i < 1: 
+                last_line = '.' * len(current_line)
+
+            for j in range(len(current_line)):
+
+                if current_line[j] in symbols: 
+            
+                    before_currline = check_line_for_digits(j, last_line) 
+                    the_currline = check_currentline_for_digits(j, current_line)
+                    after_currline = check_line_for_digits(j, next_line)
+                    # new_line: str, symbol_numbers: list[int]
+
+                    # Append the coordinates of the symbol to the key
+                    # that corresponds to the number of digits beside them in each line
+                    # Their key can not exceed 2 yet
+
+                    sums += sum(before_currline[1]) + sum(the_currline[1]) + sum(after_currline[1])
+
+            last_line = current_line
+
+            i += 1
+
+    print(sums)
+
+## How the solution works
+# 1. Scan each line for symbols
+# 2. If symbol is found, scan around the symbol
+#       starting from top (before the line where symbol is at)
+#       current line (line where symbol is at)
+#       bottom (after the line where symbol is at)
+# 3. If digits are found, convert them to '.' and put the digits in a list
+# Each symbol will have 3 function calls for top digits, current digits, bottom digits
