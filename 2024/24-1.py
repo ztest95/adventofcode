4c14ec4962fe204454ef0e87ff6ac29bc76f1f1d
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-02 01:44:43 +0800

    docs: add

--- 2024/24-1.py
+++ 2024/24-1.py
@@ -1,7 +1,10 @@
 
-def solve_star_1(input):
-    a, b = [], []
+from collections import Counter
 
+def solve_star_1(input: list[str]) -> int:
+    
+    # Parse into two lists
+    a, b = [], []
     for i in range(len(input)):
         temp = input[i].split("   ")
         a.append(int(temp[0]))
@@ -10,6 +13,7 @@ def solve_star_1(input):
     a.sort()
     b.sort()
 
+    # Calculate the sum of the absolute differences
     sum_of_diff = 0
     for i in range(len(a)):
         sum_of_diff += abs(a[i] - b[i])
@@ -17,16 +21,19 @@ def solve_star_1(input):
     return sum_of_diff
 
 
-def solve_star_2(input):
+def solve_star_2(input: list[str]) -> int:
+    
+    # Parse into two lists
     a, b = [], []
-
     for i in range(len(input)):
         temp = input[i].split("   ")
         a.append(int(temp[0]))
         b.append(int(temp[1].strip("\n")))
 
+    # Create a Counter
+    # This could also be done using collections.Counter, which is much faster
     b_counter = {}
-    keys_set = set()
+    keys_set = set() # Use set instead of dict.keys() for faster lookups
     for i in b:
         if i not in keys_set: 
             b_counter[i] = 1
@@ -34,6 +41,7 @@ def solve_star_2(input):
         else:
             b_counter[i] += 1
             
+    # Calculate the similarity score
     sim_score = 0
     for ele in a:
         if ele in keys_set:
