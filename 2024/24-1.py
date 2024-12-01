26ea68015045770b58bd9ccefaa491075df34867
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-02 01:41:11 +0800

    24-1: star 2

--- 2024/24-1.py
+++ 2024/24-1.py
@@ -1,27 +1,53 @@
-def parse_data(data):
-    pass
 
-if __name__ == "__main__":
+def solve_star_1(input):
+    a, b = [], []
 
-    a = []
-    b = []
-
-    with open("2024/input.txt") as f:
-        lines = f.readlines()
-        
-        for line in lines:
-            q, w = line.split("   ")
-            a.append(int(q))
-            b.append(int(w.strip("\n"))) 
-
+    for i in range(len(input)):
+        temp = input[i].split("   ")
+        a.append(int(temp[0]))
+        b.append(int(temp[1].strip("\n")))
+    
     a.sort()
     b.sort()
-    
+
     sum_of_diff = 0
     for i in range(len(a)):
         sum_of_diff += abs(a[i] - b[i])
 
-    print(sum_of_diff)
+    return sum_of_diff
 
 
-    
+def solve_star_2(input):
+    a, b = [], []
+
+    for i in range(len(input)):
+        temp = input[i].split("   ")
+        a.append(int(temp[0]))
+        b.append(int(temp[1].strip("\n")))
+
+    b_counter = {}
+    keys_set = set()
+    for i in b:
+        if i not in keys_set: 
+            b_counter[i] = 1
+            keys_set.add(i)
+        else:
+            b_counter[i] += 1
+            
+    sim_score = 0
+    for ele in a:
+        if ele in keys_set:
+            sim_score += ele * b_counter[ele]
+        else:
+            sim_score += 0
+
+    return sim_score
+
+
+if __name__ == "__main__":
+
+    with open('2024/input.txt') as f:
+        input = [line for line in f.readlines()]
+
+    print(solve_star_1(input))
+    print(solve_star_2(input))
