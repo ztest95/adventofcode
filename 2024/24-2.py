f997218352ffb83e61103fd0ad5019b3156fb434
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-02 13:29:45 +0800

    solved: y2024d2 star 1

---
+++ 2024/24-2.py
@@ -0,0 +1,48 @@
+
+def solve_star_1(input: list[str]) -> int:
+
+    lst = [] * len(input)
+
+    safe_count = 0
+    for line_index in range(len(input)):
+        temp = list(map(int, input[line_index].strip("\n").split(" ")))
+
+        asc = temp[0] < temp[1]
+
+        prev = temp[0]
+        safe = True # initialize true
+        
+        if asc: #Ascending, check if item is greater than previous
+            for i in range(1, len(temp)):
+                a = temp[i] - prev
+                if a > 3 or a < 1:
+                    safe = False
+                    break
+                prev = temp[i]
+
+        else: # Descending, check if item is smaller than previous
+            for i in range(1, len(temp)):
+                a = prev - temp[i]
+                if a > 3 or a < 1:
+                    safe = False
+                    break
+                prev = temp[i]
+
+        if safe:
+            safe_count += 1
+
+    return safe_count
+
+
+def solve_star_2(input: list[str]) -> int:
+    
+            
+    pass
+
+if __name__ == "__main__":
+
+    with open('2024/input.txt') as f:
+        input = [line for line in f.readlines()]
+
+    print(solve_star_1(input))
+    # print(solve_star_2(input))
