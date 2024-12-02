e68bff0c1d8e3a6672688ca2e8ff615afde9d29c
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-02 13:39:38 +0800

    solved: y2024d2 star 2

--- 2024/24-2.py
+++ 2024/24-2.py
@@ -11,7 +11,7 @@ def solve_star_1(input: list[str]) -> int:
 
         prev = temp[0]
         safe = True # initialize true
-        
+
         if asc: #Ascending, check if item is greater than previous
             for i in range(1, len(temp)):
                 a = temp[i] - prev
@@ -35,9 +35,67 @@ def solve_star_1(input: list[str]) -> int:
 
 
 def solve_star_2(input: list[str]) -> int:
-    
-            
-    pass
+    lst = [] * len(input)
+
+    safe_count = 0
+    for line_index in range(len(input)):
+        temp = list(map(int, input[line_index].strip("\n").split(" ")))
+
+        asc = temp[0] < temp[1]
+
+        prev = temp[0]
+        safe = True # initialize true
+        
+        if asc: #Ascending, check if item is greater than previous
+            for i in range(1, len(temp)):
+                a = temp[i] - prev
+                if a > 3 or a < 1:
+                    safe = False
+                    break
+                prev = temp[i]
+
+        else: # Descending, check if item is smaller than previous
+            for i in range(1, len(temp)):
+                a = prev - temp[i]
+                if a > 3 or a < 1:
+                    safe = False
+                    break
+                prev = temp[i]
+
+        if not safe:
+            ssafe = 0
+            for i in range(len(temp)):
+                temp_2 = temp.copy()
+                temp_2.pop(i)
+
+                asc = temp_2[0] < temp_2[1]
+
+                prev = temp_2[0]
+                safe = True # initialize true
+                
+                if asc: #Ascending, check if item is greater than previous
+                    for i in range(1, len(temp_2)):
+                        a = temp_2[i] - prev
+                        if a > 3 or a < 1:
+                            safe = False
+                            break
+                        prev = temp_2[i]
+
+                else: # Descending, check if item is smaller than previous
+                    for i in range(1, len(temp_2)):
+                        a = prev - temp_2[i]
+                        if a > 3 or a < 1:
+                            safe = False
+                            break
+                        prev = temp_2[i]
+
+                if safe:
+                    break
+
+        if safe:
+            safe_count += 1
+
+    return safe_count
 
 if __name__ == "__main__":
 
@@ -45,4 +103,4 @@ if __name__ == "__main__":
         input = [line for line in f.readlines()]
 
     print(solve_star_1(input))
-    # print(solve_star_2(input))
+    print(solve_star_2(input))
