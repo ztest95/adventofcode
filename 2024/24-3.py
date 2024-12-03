bc7f030b956238016647a094696d824d09794225
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-03 13:29:26 +0800

    solved: y2024d3 star 1

---
+++ 2024/24-3.py
@@ -0,0 +1,34 @@
+import re
+
+def solve_star_1(input: list[str]) -> int:
+    
+    all_matches = []
+    for line_index in  range(len(input)):
+        regex = "mul\((\d*\,\d*)\)"
+        matches = re.findall(regex, input[line_index])
+
+        all_matches = all_matches + matches
+    
+    muls = []
+    for ele in all_matches:
+        a, b = map(int, ele.split(","))
+        muls.append(a * b)
+
+    res = 0
+    for i in range(len(muls)):
+        res += muls[i]
+
+    return res
+
+
+def solve_star_2(input: list[str]) -> int:
+    res = 0
+    return res
+
+if __name__ == "__main__":
+
+    with open('2024/input.txt') as f:
+        input = [line for line in f.readlines()]
+
+    print(solve_star_1(input))
+    print(solve_star_2(input))
