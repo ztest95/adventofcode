845b0fcee981e5004e2e527ab5f721dc6023db65
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-05 14:48:39 +0800

    solved: y2024d05 star 1

--- 2024/24-5.py
+++ 2024/24-5.py
@@ -3,82 +3,83 @@ class Node:
         self.data = data
         self.next = []
 
+    def next_data(self):
+        return [node.data for node in self.next]
+
+def topological_sort(node: Node, visited: set, stack: list[Node]):
+    visited.add(node)
+    for next_node in node.next:
+        if next_node not in visited:
+            topological_sort(next_node, visited, stack)
+    stack.append(node)
+    
+    return stack
+
 def add_next(node_a, node_b):
     
     for node in node_a.next:
         if node.data == node_b.data:
             return 
         
-    node_a.next.add(node_b)
-
-# def solve_star_1(input: list[str]) -> int:
-#     res = 0
-
-#     sec1_end_idx = 0
-#     start_nodes = [] # cant use set since i need to get 
-#     for line_index in range(len(input)):
-#         if input[line_index] == '':
-#             sec1_end_idx = line_index
-#             break
-#         input[line_index]
-#         a, b = input[line_index].split('|')
-#         node_a = Node(a)
-#         node_b = Node(b)
-#         for node in start_nodes:
-#             if node.data == a: # if node already in list, add node b as next
-#                 node.next.append(node_b)
-#                 break
-        
-#         # if node not in list, add node a and b
-#         else:
-#             node_a.next.append(node_b)
-#             start_nodes.append(node_a)
-
-    
-#     # for node in start_nodes:
-#     #     print(node.data)
-#     #     print(node.next)
-
-#     sec2 = input[sec1_end_idx+1:]
-
-#     print(start_nodes)
-#     complete_rules = []
-#     for node in start_nodes:
-#         complete_rules.append(node.data)
-#         for next_node in node.next:
-#             complete_rules.append(next_node.data)
-
-#     cr_set = set()
-#     for i in range(len(complete_rules)):
-#         if complete_rules[i] in cr_set:
-#             complete_rules[i] = ''
-#         else:
-#             cr_set.add(complete_rules[i])
-    
-#     iter_n = len(complete_rules) - len(cr_set)
-
-#     while iter_n > 0:
-#         complete_rules.remove('')
-#         iter_n -= 1
-        
-
-#     # for line_index in range(len(sec2)):
-#     #     pages_lst = sec2[line_index].split(',')
-        
-#     #     for p_num in pages_lst:
-
-            
-#     #         for node in start_nodes:
-#     #             if node.data == p_num:
-#     #                 break
-#     #         else:
-#     #             node = Node(p_num)
-#     #             start_nodes.append(node)
-
-    # return res
+    node_a.next.append(node_b)
 
 def solve_star_1(input: list[str]) -> int:
     res = 0
+
+    sec1_end_idx = 0
+    start_nodes = [] # cant use set since i need to get 
+    nums = set()
+    rules = []
+    for line_index in range(len(input)):
+        level_2 = set()
+        if input[line_index] == '':
+            sec1_end_idx = line_index
+            break
+        input[line_index]
+        a, b = input[line_index].split('|')
+        rules.append((a, b))
+        nums.add(a)
+        nums.add(b)
+    
+    nums = list(nums)
+    for num in nums:
+        start_nodes.append(Node(num))
+
+    mapp = dict(zip(nums, start_nodes))
+    for rule in rules:
+        a = rule[0]
+        b = rule[1]
+        node_a = mapp[a]
+        node_b = mapp[b]
+        add_next(node_a, node_b)
+
+    sec2 = input[sec1_end_idx+1:]
+    
+    upds = []
+    for line in sec2:
+        upds.append(line.split(','))
+    
+    
+    for upd in upds:
+        fail = False
+        print("====LINE: ", upd)
+        aa = []
+        for page in upd:
+            copy = upd.copy()
+            if page in nums:
+                aa.append(page)
+                after = list[set(mapp[page].next_data()) - set(copy)]
+                print('visited', aa)
+
+        for i in range(len(aa)-1, 0, -1):
+            before = set(aa[:i])
+            for ele in mapp[aa[i]].next_data():
+                if ele in before:
+                    fail = True
+                    break
+        
+        if not fail:
+            res += int(aa[int(len(aa)/2)])
 
     return res
 
@@ -88,7 +89,7 @@ def solve_star_2(input: list[str]) -> int:
 
 if __name__ == "__main__":
 
-    with open('2024/testinput.txt') as f:
+    with open('2024/input.txt') as f:
         input = [line.strip('\n') for line in f.readlines()]
 
     print(solve_star_1(input))
