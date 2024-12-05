584db3e31d2831c86118bcb8c9f1ecfb445fe858
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-06 05:11:10 +0800

    chore: clean solution

--- 2024/24-5.py
+++ 2024/24-5.py
@@ -71,21 +71,11 @@ def solve_star_1(input: list[str]) -> int:
     return res
 
 def solve_star_2(input: list[str]) -> int:
-    # my initial idea was that, i could create a list of all the values that follows all the rules...
-    # iterating through that list and appending whenever the val exists in updates resulting in the ordered_update.
-    #   all_vals_sorted = [] 
-    #   ordered_update = []
-    #   for ele in all_vals_sorted: # iterate universally sorted graph
-    #   if ele in update: # for each ele, check if its in an update
-    #   ordered_update.append(ele) # add new (ordered) list
-    # this worked on the test input which has like 5 pages each update, but not on actual input. 
-    # took me so much time to debug and try a diff approach 
-    
+    # sort each update based on rules
+
     res = 0
 
     sec1_end_idx = 0
-    start_nodes = [] 
-    nums = set()
     rules = [] # list of rules "Before|After"
     for line_index in range(len(input)):
         if input[line_index] == '':
@@ -95,63 +85,24 @@ def solve_star_2(input: list[str]) -> int:
         input[line_index]
         a, b = input[line_index].split('|')
         rules.append((a, b))
-        nums.add(a)
-        nums.add(b)
-    
-    # Create list for each number
-    nums = list(nums)
-    for num in nums:
-        start_nodes.append(Node(num))
-
-    mapp = dict(zip(nums, start_nodes))
-    for rule in rules:
-        a = rule[0]
-        b = rule[1]
-        node_a = mapp[a]
-        node_b = mapp[b]
-        add_next(node_a, node_b)
 
     # List for each update
     upds = []
     for line in input[sec1_end_idx+1:]:
         upds.append(line.split(','))
-
-    indeg_count = {node: 0 for node in start_nodes}
-    for node in start_nodes:
-        for next_node in node.next:
-            indeg_count[next_node] += 1
-
-    zero_indeg = [node for node in start_nodes if indeg_count[node] == 1]
-
-    def topo_sort(S: list[Node], l: list[Node] = None) -> list:
-        if l is None:
-            l = []
-        if not S:
-            return l
-        v = S.pop()
-        l.append(v)
-        for w in v.next:
-            indeg_count[w] -= 1
-            if indeg_count[w] == 0:
-                S.append(w)
-
-        return topo_sort(S, l)
     
     def sort(S: list[Node]) -> list:
         while True:
-            is_sorted = True
+            is_sorted = True # assume sorted
             i = 0
             while i < len(S) - 1:
-                if (S[i+1], S[i]) in rules:
-                    is_sorted = False
-                    S[i], S[i+1] = S[i+1], S[i]
-                i += 1  # Increment i in each iteration
+                if (S[i+1], S[i]) in rules: # if flipped
+                    is_sorted = False # not sorted
+                    S[i], S[i+1] = S[i+1], S[i] # and swap
+                i += 1  
             
             if is_sorted:
                 return S
-            
-    final_seq = topo_sort(zero_indeg)
-    final_seq = sort(start_nodes)
 
     for upd in upds:
         ordered = []
