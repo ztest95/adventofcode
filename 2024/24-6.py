da21ba0def115359b69a6b94a225575aa2149581
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-06 18:21:00 +0800

    notsolved: y2024d06 star 2

--- 2024/24-6.py
+++ 2024/24-6.py
@@ -90,19 +90,210 @@ def solve_star_1(input: list[str]) -> int:
         
         forward = (i, j)
 
-    # for x in input:
-    #     print(x)
+    for x in input:
+        print(''.join(x))
     return blocks + 1
 
 def solve_star_2(input: list[str]) -> int:
+    # i want to save the coordinates of all the wall
+    # then everytime i encounter a visited block, 
+    #       ill save this coordinate, and following coordinate
+    #       i will check if the path to the right is visited until the next block
+    #       if yes, 
+    #               ill turn right, forward, 
+    #               and continue the path with my movement rule. 
+    #               if i reach a non-visited block, 
+    #                      break this (not a loop)
+    #               if i reach the same coordinate
+    #                       then a wall in the following coordinate creates a loop
+    #                       loop + 1
+    #       if no
+    #               go back to the coordianate i saved
+    #
+    # do this until im out of bound
     res = 0
+
+    for i in range(len(input)):
+        input[i] = list(input[i])
+
+    i, j = 0, 0
+
+    faces = ['^', '>', 'v', '<']
+    faces_set = set(faces) # just to save some time checking if in\
+    init_face = 0
+
+    found = False
+    for i in range(len(input)):
+        for j in range(len(input[i])):
+            if input[i][j] in faces_set:
+                # get index of face
+                for f_idx in range(len(faces)):
+                    if input[i][j] == faces[f_idx]:
+                        init_face = f_idx
+                        found = True
+                        break
+                if found:
+                    break
+        if found:
+            break
+    # input[i][j] = 'X'
+    blocks = 1
+    max_i = len(input)  
+    max_j = len(input[0])
+    curr_idx = (i, j)
+    curr = input[i][j] # set starting position to curr
     
-    return res
+    # get the coordinate of faced block
+    if init_face == 0:
+        i -= 1
+    elif init_face == 1:
+        j += 1
+    elif init_face == 2:
+        i += 1
+    elif init_face == 3:
+        j -= 1
+    forward = (i, j) # set forward to the faced block
+
+    loops = 0
+    while i < max_i and j < max_j:
+
+        next = input[forward[0]][forward[1]]
+
+        for x in input:
+            print(''.join(x))
+        print(blocks, forward, curr_idx)
+        # if current position not visited
+        if input[curr_idx[0]][curr_idx[1]] == '.' or input[curr_idx[0]][curr_idx[1]] in faces_set:
+            blocks += 1 
+            input[curr_idx[0]][curr_idx[1]] = 'X' # mark as visited
+        # do nothing if already visited
+
+        elif input[curr_idx[0]][curr_idx[1]] == 'X':
+            print("VISITED", curr_idx)
+            loop_curr_idx = (curr_idx[0], curr_idx[1])    # save the current index
+            loop_face = (init_face + 1) % 4 # change face
+            
+            loop_i, loop_j = loop_curr_idx
+            if loop_face == 0:  
+                # loop_j += 1
+                loop_i -= 1
+            elif loop_face == 1:
+                # loop_i += 1
+                loop_j += 1
+            elif loop_face == 2:
+                # loop_j -= 1
+                loop_i += 1
+            elif loop_face == 3:
+                # loop_i -= 1
+                loop_j -= 1
+            loop_forward = (loop_i, loop_j)
+            
+            not_a_loop = False
+            # After this, guard is temporary rotated
+            # Standing at the intersection block, and Next block is the forward block
+            print("CURRENTLY AT", loop_curr_idx, "NEXT", loop_forward)
+            while loop_forward != curr_idx and not not_a_loop:
+                print("loop_curr_index", loop_curr_idx, "loop_next", loop_forward)
+                loop_next = input[loop_forward[0]][loop_forward[1]]
+                if loop_next == ".":
+                    not_a_loop = True
+                    break
+
+                if loop_next == "#":
+                    print("NEXT BLOCK IS WALL ENCOUNTERD")
+                    print('curr', loop_curr_idx, 'next', loop_forward)
+                    loop_face = (loop_face + 1) % 4 # rorate
+                    # get the next block
+
+                    loop_curr_idx = loop_forward
+                    # input[loop_curr_idx[0]][loop_curr_idx[1]] = '|'
+                    print("LOOP_FACE", faces[loop_face])
+                    print("LOOP FORWARD", loop_forward) 
+                    if loop_face == 0:  
+                        loop_j += 1
+                        loop_i -= 1
+                    elif loop_face == 1:
+                        loop_i += 1
+                        loop_j += 1
+                    elif loop_face == 2:
+                        loop_i += 1
+                        loop_j -= 1
+                    elif loop_face == 3:
+                        loop_i -= 1
+                        loop_j -= 1
+                    
+                    loop_forward = (loop_i, loop_j)
+                    print("NEW LOOP FORWARD", loop_forward)
+                    continue
+                
+                loop_curr_idx = loop_forward
+                # input[loop_curr_idx[0]][loop_curr_idx[1]] = '|'
+
+                if loop_face == 0:  
+                    # loop_j += 1
+                    loop_i -= 1
+                elif loop_face == 1:
+                    # loop_i += 1
+                    loop_j += 1
+                elif loop_face == 2:
+                    # loop_j -= 1
+                    loop_i += 1
+                elif loop_face == 3:
+                    # loop_i -= 1
+                    loop_j -= 1
+
+                loop_forward = (loop_i, loop_j)
+            print("VISITED STILL", curr_idx)
+            print("LOOP CURR IDX", loop_curr_idx)
+            print("=====================================")
+            if not not_a_loop:
+                print("LOOP FOUND")
+                loops += 1
+ 
+
+        # Check Next Block
+        if next == '#': # if wall
+            init_face = (init_face + 1) % 4 # rotate gurad
+            # get the next block
+            if init_face == 0:  
+                j += 1
+                i -= 1
+            elif init_face == 1:
+                i += 1
+                j += 1
+            elif init_face == 2:
+                j -= 1
+                i += 1
+            elif init_face == 3:
+                i -= 1
+                j -= 1
+
+            forward = (i, j)
+
+        # Move to next block
+        # put next to curr
+        curr_idx = forward
+        # update next index
+        if init_face == 0:
+            i -= 1
+        elif init_face == 1:
+            j += 1
+        elif init_face == 2:
+            i += 1
+        elif init_face == 3:
+            j -= 1
+        
+        forward = (i, j)
+        # next will be updated in the next iteration
+
+    for x in input:
+        print(''.join(x))
+    return loops
 
 if __name__ == "__main__":
 
-    with open('2024/input.txt') as f:
+    with open('2024/testinput.txt') as f:
         input = [line.strip('\n') for line in f.readlines()]
-
+    x = input.copy()
     print(solve_star_1(input))
-    print(solve_star_2(input))
+    print(solve_star_2(x))
