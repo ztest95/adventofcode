1b3acf6afa7aed3ab225bdec88f17de5d1dbf576
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-08 03:50:12 +0800

    docs: add comments on y2024d04 solution

--- 2024/24-4.py
+++ 2024/24-4.py
@@ -1,78 +1,85 @@
+import re
+
+
 def solve_star_1(input: list[str]) -> int:
     res = 0
 
+    # Counting Horizontal and Vertical "XMAS"
+
+    input = input.copy()
     # COUNT ALL HORIZONTAL -> RIGHTWARD
     for row_idx in range(len(input)):
         count = re.findall(r'XMAS', input[row_idx])
         res += len(count)
-    # print("Horizontal-Right: ", res)
 
-    # COUNT ALL HORIZONTAL -> LEFTWARD
-    reversed_input = [line[::-1] for line in input]
+    # Reverse Horizontal and COUNT ALL HORIZONTAL -> LEFTWARD   
+    reversed_input = [line[::-1] for line in input] # create a reversed list that will be used later
     for row_idx in range(len(input)):
         count = re.findall(r'XMAS', reversed_input[row_idx])
         res += len(count)
-    # print("Horizontal-Left: ", res)
 
+    # Rotate list, making vertical to horizontal
     shifted_list = []
-    new = input.copy()
-    for line_index in range(len(new)):
-        new[line_index] = new[line_index].strip('\n')
-    
-    shifted_list = []
-    cols = len(new[0])
-    rows = len(new)
+    cols = len(input[0])
+    rows = len(input)
 
-    for i in range(cols):
-        shifted_list.append(''.join([new[j][i] for j in range(rows)]))
+    # Convert List to Vertical
+    shifted_list = [''.join([input[j][i] for j in range(rows)]) for i in range(cols)]
 
-    # Count all VERTICAL 
+    # Count all 'VERTICAL', 'UPWARD' and 'DOWNWARD'
     for row_idx in range(len(shifted_list)):
         count_right = re.findall(r'XMAS', shifted_list[row_idx])
         count_left = re.findall(r'XMAS', shifted_list[row_idx][::-1])
         res += len(count_right) + len(count_left)
-    # print("Vertical: ", res)
 
-    # Convert List to Diagonal
+    # Counting Diagonal "XMAS"
 
-    cols = len(shifted_list[0])
-    rows = len(shifted_list)
-    diagonal_list = []
+    # Creating Diagonal List
+    
+    # Visit each element in the list and append to the diagonal list
+    # ex.
+    # 1, 3, 6
+    # 2, 5, 8 
+    # 4, 7, 9
+
+    cols = len(input[0])
+    rows = len(input)
 
     diagonal_list = [ [] for i in range(rows + cols - 1) ]
-    for x in range(rows):
-        for i in range(cols):
-            char = shifted_list[x][i]
-            diagonal_list[x+i].append(char)
-    
-    for i in range(len(diagonal_list)):
-        diagonal_list[i] = ''.join(diagonal_list[i])
+    for y in range(rows):
+        for x in range(cols):
+            diagonal_list[y+x].append(input[y][x])
 
-    # READ DIAGONAL ASCENDING
+    # Convert List to String
+    diagonal_list = [''.join(diagonal_list[i]) for i in range(len(diagonal_list))]
+
+    # Count all 'DIAGONAL', 'BOTTOM LEFT to TOP RIGHT' and 'TOP RIGHT to BOTTOM LEFT'
     for row_idx in range(len(diagonal_list)):
         count_right = re.findall(r'XMAS', diagonal_list[row_idx])
         count_left = re.findall(r'XMAS', diagonal_list[row_idx][::-1])
         res += len(count_right) + len(count_left)
-    # print("Bottom Left to Top Right: ", res)
     
-    reverse = []
-    for i in range(len(shifted_list)):
-        reverse.append(shifted_list[i][::-1])
+    # Creating the other Diagonal List
     
+    # Visit each element in the list and append to the diagonal list
+    # ex.
+    # 6, 3, 1
+    # 8, 5, 2 
+    # 9, 7,4
+
     diagonal_list_2 = [ [] for i in range(rows + cols - 1) ]
     for x in range(rows):
         for i in range(cols):
-            char = reverse[x][i]
+            char = reversed_input[x][i] # use the reversed list
             diagonal_list_2[x+i].append(char)
     
-    for i in range(len(diagonal_list_2)):
-        diagonal_list_2[i] = ''.join(diagonal_list_2[i])
+    diagonal_list_2 = [''.join(diagonal_list_2[i]) for i in range(len(diagonal_list_2))]
 
+    # Count all 'DIAGONAL', 'BOTTOM RIGHT to TOP LEFT' and 'TOP LEFT to BOTTOM RIGHT'
     for row_idx in range(len(diagonal_list_2)):
         count_right = re.findall(r'XMAS', diagonal_list_2[row_idx])
         count_left = re.findall(r'XMAS', diagonal_list_2[row_idx][::-1])
         res += len(count_right) + len(count_left)
-    # print("Bottom Right to Top Left: ", res)
 
     return res
 
@@ -81,22 +88,25 @@ def solve_star_2(input: list[str]) -> int:
 
     for i, current_line in enumerate(input):
 
+        # check every "A"
         for a in range(len(current_line)):
+            # check if A is not at the edge
             if current_line[a] == 'A' and (a != 0 and a != len(current_line.strip('\n'))-1) and (i != 0 and i < len(input)-1):
                 around_1 = input[i-1][a-1] + 'A' + input[i+1][a+1]
                 around_2 = input[i-1][a+1] + 'A' + input[i+1][a-1]
-                
+                # check "X-MAS"
                 if around_1 in ('MAS', 'SAM') and around_2 in ('MAS', 'SAM'):
                     res += 1
     return res
 
 if __name__ == "__main__":
 
-    with open('2024/input.txt') as f:
-        input = [line for line in f.readlines()]
+    with open('2024/testinput.txt') as f:
+        input = [line.strip('\n') for line in f.readlines()]
 
     print(solve_star_1(input))
     print(solve_star_2(input))
 
 # References:
+# Star 2
 # AoC 2023/day/3 
