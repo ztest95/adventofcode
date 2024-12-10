ec81bf5f9796a22b5d95dd27eb52c1e5c1b6a807
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-10 15:51:27 +0800

    feat: add solution file template

---
+++ 2024/example.py
@@ -0,0 +1,22 @@
+def solve_star_1(input: list[str]) -> int:
+    # solution idea:
+    
+    res = 0
+
+    return res
+
+
+def solve_star_2(input: list[str]) -> int:
+    # solution idea
+
+    res = 0
+
+    return res
+
+if __name__ == "__main__":
+
+    with open('2024/input.txt') as f:
+        input = [line.strip('\n') for line in f.readlines()]
+    
+    print(solve_star_1(input))
+    print(solve_star_2(input.copy()))
