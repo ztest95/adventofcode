aa75fee1276cdc6dd1938afd620017113eb0e03a
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-12 04:44:30 +0800

    notsolved: y2024d12 star 2 having troubles writing recursion

--- 2024/24-11.py
+++ 2024/24-11.py
@@ -65,16 +65,60 @@ def solve_star_1(input: list[str]) -> int:
 
 
 def solve_star_2(input: list[str]) -> int:
-    # solution idea
-
+    # try to make it faster whiel waiting
     res = 0
 
-    return res
+    stones = list(map(len ,input[0].split(' ')))
+
+    stones =  input[0].split(' ')
+    for i in range(len(stones)):
+        stones[i] = (len(stones[i]), stones[i])
+
+    # print(stones)
+    ITER = 6
+    def calc(num_str, i, n_split=0):
+        
+        if i == 5:
+            return n_split
+        
+        if len(num_str) % 2 == 0:
+            l_half = num_str[:len(num_str)//2]
+            r_half = '0' if len(num_str[len(l_half):].lstrip('0')) == 0 else num_str[len(l_half):]
+            print('num_str:', num_str, 'splitting:' ,'l_half:', l_half, 'r_half:', r_half)
+            a = calc(l_half, i+1, 0) 
+            b = calc(r_half, i+1, 0) 
+            n_split += 1
+            if a:
+                n_split += a
+            if b:
+                n_split += b
+        elif num_str == '0':
+            c = calc('1', i+1, 0) 
+            if c:
+                n_split += c
+        else:
+            d = calc(str(int(num_str) * 2024), i+1, 0) 
+            if d:
+                n_split += d
+        
+        return n_split
+    
+    temp = []
+    for stone in stones:
+        print(stone)
+        temp.append(calc(stone[1], 0, 0))
+        print(temp)
+
+    print(temp)
+    for x in temp:
+        res += x
+
+    return res + len(stones)
 
 if __name__ == "__main__":
 
-    with open('2024/input.txt') as f:
+    with open('2024/testinput.txt') as f:
         input = [line.strip('\n') for line in f.readlines()]
     
-    print(solve_star_1(input))
+    # print(solve_star_1(input))
     print(solve_star_2(input.copy()))
