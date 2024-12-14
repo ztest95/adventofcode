6e5cb74eed21de9eb303daa8f8a2a30aa6567c8a
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-14 15:33:23 +0800

    chore: clean solution

--- 2024/24-14.py
+++ 2024/24-14.py
@@ -50,6 +50,9 @@ def solve_star_2(input: list[str]) -> int:
     # solution idea:
     # use a for loop to iterate positions of robots at each loop
     # i think if left and right quadraants are equal, its a christmas three
+    # edit, after watching my cli for a while for each 100 sec cycle
+    # i looked online for answers,
+    # and decided to use save each arr as image
     res = 0
 
     ## example:
@@ -66,11 +69,8 @@ def solve_star_2(input: list[str]) -> int:
         p = tuple(map(int, re.findall(r'\d+,\d+', p)[0].split(',')))
         v = tuple(map(int, re.findall(r'-?\d+,-?\d+', v)[0].split(',')))
         input[line_idx] = [p, v]
-    
-    print("PART 2")
+        
     for i in range(9999):
-        print(i)
-        # print("======================================================================================================")
         q1, q2, q3, q4 = 0, 0, 0, 0
         sset = set()
 
@@ -103,77 +103,14 @@ def solve_star_2(input: list[str]) -> int:
                 else:
                     q3 += 1
 
-        # # get quadrants
-        # if q1 == q2 and q3 == q4 and q1 != 0: # board is inversed
-        #     print('found christmas tree')
-        #     res = i
-        #     break
-        # creating tree
         for coord in sset:
             board[coord[0]][coord[1]] = True
-        # time.sleep(1)
-        # for line in board:
-            # print(line)
-            # print("".join(line))
 
         np_arr = board
         image_arr = np.where(np_arr, 255, 0).astype(np.uint8)
         
-        if i > 4658:
-            img = Image.fromarray(image_arr , 'L')
-            img.save(f"output_{i}.png")
-    # print("PART 2")
-    # i = 0
-    # while res == 0:
-    #     print(i)
-    #     q1, q2, q3, q4 = 0, 0, 0, 0
-    #     sset = set()
-
-    #     board = []  
-    #     for x in range(COL_MAX):
-    #         line = []
-
-    #         for y in range(ROW_MAX):
-    #             line.append('.')
-
-    #         board.append(line)
-        
-    #     for x in input:
-    #         p = x[0]
-    #         v = x[1]
-
-    #         p = ((p[0] + (v[0])) % COL_MAX , (p[1] + (v[1])) % ROW_MAX)
-    #         # print(p)
-    #         sset.add(p)
-
-    #         if p[0] < COL_MAX // 2:
-    #             if p[1] < ROW_MAX // 2:
-    #                 q1 += 1
-    #             else:
-    #                 q4 += 1
-    #         else:
-    #             if p[1] < ROW_MAX // 2:
-    #                 q2 += 1
-    #             else:
-    #                 q3 += 1
-
-    #     # get quadrants
-    #     if q1 == q2 and q3 == q4 and q1 != 0: # board is inversed
-    #         print('found christmas tree')
-    #         res = i
-    #         break
-
-    #     i += 1
-
-    #     for coord in sset:
-    #         board[coord[0]][coord[1]] = 'X'
-
-    #     time.sleep(.25)
-    #     for line in board:
-    #             print("".join(line))
-    #     # creating tree
-
-    # return res
+        img = Image.fromarray(image_arr , 'L')
+        img.save(f"output_{i}.png")
 
 if __name__ == "__main__":
 
