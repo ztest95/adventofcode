da0c54a1f8761c9bd3075005cef4a61e51662b9b
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-12-18 17:43:51 +0800

    solved: y2024d18 star 1

--- 2024/24-18.py
+++ 2024/24-18.py
@@ -1,48 +1,65 @@
+GRID_SIZE = 71
+DROPS = 1024
+
+
 def solve_star_1(input: list[str]) -> int:
     # solution idea:
-    
+
     res = 0
-    
-    grid_size = 7
-    mapp = [ [0] * grid_size for _ in range(grid_size) ]
-    print(mapp)
+
+    mapp = [[0] * GRID_SIZE for _ in range(GRID_SIZE)]
     walls = set()
-    for i in range(1024):
-        x, y = tuple(map(int, input[i].split(',')))
+    for i in range(DROPS):
+        x, y = tuple(map(int, input[i].split(",")))
         mapp[x][y] = 1
-        walls.add((x,y))
+        walls.add((x, y))
 
-
-    for i in range(len(mapp)):      
-        print(mapp[i])
-
-    res = dfs(mapp, walls, (0,0), 0)
+    res = bfs(mapp, walls, (0, 0), 0)
 
     return res
 
-def dfs(grid: list, wall: set, coord: tuple, steps: int) -> int:
+def bfs(grid: list, wall: set, coord: tuple, steps: int) -> int:
     x, y = coord[0], coord[1]
-    print(x,y)
-    if (x, y) == (6, 6):
-        print(steps)
-        return steps
 
-    dirs = {(-1,0), (0, 1), (1, 0), (0, -1)}
-    around = {(x+dx, y+dy) for dx, dy in dirs}
+    paths = [coord]
+
     walls = wall.copy()
     walls.add((x, y))
-    asd = []
-    for xx, yy in around:
-        # print(xx > 0)
-        # print(yy > 0)
-        # print(xx < 7)
-        # print(yy < 7)
-        # print((xx,yy) not in wall)
-        if xx > -1 and yy > -1 and xx < 7 and yy < 7 and (xx, yy) not in wall:
-            asd.append(dfs(grid, walls, (xx, yy), steps + 1))
-        else:
-            asd.append(10000)
-    return min(asd) if around else 9 ** 10
+
+    dirs = {(-1, 0), (0, 1), (1, 0), (0, -1)}
+
+    found = False
+    new_layer_count = 0
+    curr_layer_count = len(paths)
+    while curr_layer_count > 0:
+        x, y = paths.pop(0)
+        if (x, y) == (GRID_SIZE - 1, GRID_SIZE - 1):
+            found = True
+            break
+
+        around = {(x + dx, y + dy) for dx, dy in dirs}
+        for xx, yy in around:
+            if (
+                xx > -1
+                and yy > -1
+                and xx < GRID_SIZE
+                and yy < GRID_SIZE
+                and (xx, yy) not in walls
+            ):
+                paths.append((xx, yy))
+                walls.add((xx, yy))
+                new_layer_count += 1
+
+        curr_layer_count -= 1
+        if curr_layer_count == 0:
+            curr_layer_count = new_layer_count
+            new_layer_count = 0
+            steps += 1
+
+    if found:
+        return steps
+
+    return -1
 
 
 def solve_star_2(input: list[str]) -> int:
@@ -52,10 +69,15 @@ def solve_star_2(input: list[str]) -> int:
 
     return res
 
+
 if __name__ == "__main__":
 
-    with open('2024/24-18test.txt') as f:
-        input = [line.strip('\n') for line in f.readlines()]
-    
+    with open("2024/24-18.txt") as f:
+        input = [line.strip("\n") for line in f.readlines()]
+
     print(solve_star_1(input))
     print(solve_star_2(input.copy()))
+
+# References
+# https://youtu.be/KiCBXu4P-2Y
+# this is actually my first time impementing bfs
